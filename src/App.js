cabead9f0a4a0ab9c9542847f458feb62190fe22
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-07-18 14:55:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,45 +5,49 @@ import Weather from "./Weather";
 import Error from "./Error";
 import Loading from "./Loading";
 
+const App = () => {
+  const [city, setCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false); // Initialize to false instead of null
 
-const App = ()=>{
-    const [city, setCity] = useState("");
-    const [weatherInfo, setWeatherInfo] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
+  const handleCityInput = (e) => {
+    setCity(e.target.value);
+  };
 
-    const handleCityInput = (e)=>{
-        setCity(e.target.value);
+  const handleGetWeather = async () => {
+    setLoading(true);
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f08cd5f06fb6a6e5f97490d572b65096&units=metric`;
+      const getData = await axios.get(url);
+      const data = getData.data;
+      setWeatherInfo(data);
+      setError(null);
+      setLoading(false); // Set loading to false after successful data fetch
+    } catch (err) {
+      setLoading(false); // Set loading to false on error as well
+      setWeatherInfo(null);
+      setError(err.message);
     }
+  };
 
-    const handleGetWeather = async ()=>{
-        setLoading(true);
-        try{
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f08cd5f06fb6a6e5f97490d572b65096&units=metric`;
-            const getData = await axios.get(url);
-            const data = getData.data;
-            setWeatherInfo(data);
-            setError(null);
-            setLoading(null);
-        }catch(err){
-            setLoading(null);
-            setError(err.message);
-        }
-    }
-
-    return(
-        <>
-            <div className="location-input-div">
-                    <input type="text" placeholder="Enter Your City......" onChange={handleCityInput}/>
-            </div>
-            <div className="button-div">
-                <button onClick={handleGetWeather}>GET INFO</button>
-            </div>
-            {weatherInfo && <Weather weatherInfo={weatherInfo}/>}
-            {error && <Error error={error}/>}
-            {loading && <Loading loading={"Loading..."}/>}
-        </>
-    )
-}
+  return (
+    <>
+      <div className="location-input-div">
+        <input
+          type="text"
+          placeholder="Enter Your City......"
+          onChange={handleCityInput}
+        />
+      </div>
+      <div className="button-div">
+        <button onClick={handleGetWeather}>GET INFO</button>
+      </div>
+      {weatherInfo && <Weather weatherInfo={weatherInfo} />}
+      {error && <Error error={error} />}
+      {loading && <Loading loading={"Loading..."} />}
+    </>
+  );
+};
 
 export default App;
